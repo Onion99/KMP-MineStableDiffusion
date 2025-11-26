91f28a5a91907ab66adfd387b87628b72e82162b
OnionTiger <lijundong@inke.cn>
2025-11-26 19:01:26 +0800

    feat: JNI搭建StableDiffusion方法桥梁

--- composeApp/src/desktopMain/kotlin/org/onion/diffusion/native/DiffusionLoader.desktop.kt
+++ composeApp/src/desktopMain/kotlin/org/onion/diffusion/native/DiffusionLoader.desktop.kt
@@ -9,11 +9,42 @@ actual class DiffusionLoader actual constructor() {
     init {
         NativeLibraryLoader.loadFromResources("sdloader")
     }
+    private var nativePtr = 0L
 
     actual suspend fun getModelFilePath(): String {
         return FileKit.openFilePicker()?.file?.absolutePath ?: ""
     }
 
+
     actual fun loadModel(modelPath: String) {
+        nativePtr = nativeLoadModel(modelPath,true,true,true)
     }
+
+    actual fun release() {
+        nativeRelease(nativePtr)
+    }
+
+    actual fun txt2Img(
+        handle: Long,
+        prompt: String,
+        negative: String,
+        width: Int,
+        height: Int,
+        steps: Int,
+        cfg: Float,
+        seed: Long
+    ): ByteArray? = nativeTxt2Img(handle,prompt,negative,width,height,steps,cfg,seed)
+
+    private external fun nativeLoadModel(modelPath: String, offloadToCpu: Boolean, keepClipOnCpu: Boolean, keepVaeOnCpu: Boolean): Long
+    private external fun nativeTxt2Img(
+        handle: Long,
+        prompt: String,
+        negative: String,
+        width: Int,
+        height: Int,
+        steps: Int,
+        cfg: Float,
+        seed: Long
+    ): ByteArray?
+    private external fun nativeRelease(handle: Long)
 }
